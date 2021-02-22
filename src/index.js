c6a7acf3b09fbc24b10fe59132012767d96b45fa
maryd-k <maryia.dyliuk@gmail.com>
2021-02-22 15:22:14 +0300

    feat: add functions: max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) 
+        return 0;
+    else
+    {
+        let minValue = array[0];
+        for (let i=0; i<array.length; i++)
+        {
+            if (array[i] < minValue)
+            minValue = array[i];
+        }
+        return minValue;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) 
+    return 0;
+  else
+  {
+    let maxValue=array[0];
+    for (let i=0; i<array.length; i++)
+    {
+      if (array[i] > maxValue)
+      maxValue = array[i];
+    }
+    return maxValue;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0)  
+        return 0;
+    else
+    {
+        let sum = 0;
+        for (let i=0; i<array.length; i++)
+        {
+            sum=sum+array[i];
+        }
+        return sum/array.length;
+    }
 }
